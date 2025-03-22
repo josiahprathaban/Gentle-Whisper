82b0b988d6ad78dcdc985a766b2cc2ec22ea9dc6
josiahprathaban <josiah.prathaban@gmail.com>
2025-03-22 20:20:01 +0530

    add test btn

--- vite.config.mjs
+++ vite.config.mjs
@@ -6,6 +6,7 @@ import Layouts from 'vite-plugin-vue-layouts'
 import Vue from '@vitejs/plugin-vue'
 import VueRouter from 'unplugin-vue-router/vite'
 import Vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // Utilities
 import { defineConfig } from 'vite'
@@ -26,6 +27,7 @@ export default defineConfig({
         configFile: 'src/styles/settings.scss',
       },
     }),
+    VitePWA({ registerType: 'autoUpdate' }),
     Components(),
     Fonts({
       google: {
